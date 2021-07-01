fd42c671dad9e97bc31e78e23756356c9957f019
wnsgk8300 <wnsgk8300@gmail.com>
2021-07-01 13:08:13 +0900

    tableView

---
+++ DreamKurly/DreamKurly/Views/RecommendTableViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  RecommendTableViewCell.swift
+//  DreamKurly
+//
+//  Created by JEON JUNHA on 2021/06/30.
+//
+
+import SnapKit
+import UIKit
+
+class RecommendTableViewCell: UITableViewCell {
+    
+    static let identifier = "RecommendTableViewCell"
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+extension RecommendTableViewCell
+{
+    
+    final private func setUI() {
+        setBasic()
+        setLayout()
+    }
+    final private func setBasic() {
+        
+    }
+    final private func setLayout() {
+        
+    }
+}
