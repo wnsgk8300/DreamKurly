2232847e7bea15c415e17aa0de6ce0fed90b4c08
wnsgk8300 <wnsgk8300@gmail.com>
2021-07-03 12:40:49 +0900

    ....

--- DreamKurly/DreamKurly/Controllers/ViewController.swift
+++ DreamKurly/DreamKurly/Controllers/ViewController.swift
@@ -8,10 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let recommendationTableView = UITableView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    func configureTableView() {
+        recommendationTableView.backgroundColor = .clear
+        recommendationTableView.separatorStyle = .none
+        recommendationTableView.allowsSelection = false
+
+        recommendationTableView.dataSource = self
+        recommendationTableView.delegate = self
+
+        recommendationTableView.register(RecommendProductListCell.self,
+                                         forCellReuseIdentifier: RecommendProductListCell.identifier)
+        recommendationTableView.register(RecommendReviewsProductCell.self,
+                                         forCellReuseIdentifier: RecommendReviewsProductCell.identifier)
+    }
 }
 
+extension ViewController: UITableViewDelegate {
+    
+}
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+}
